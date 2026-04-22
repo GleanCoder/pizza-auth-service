4d492087afb81c23892c1637b627727ac090348b
GleanCoder <adityakiranacharya0@gmail.com>
2026-04-22 09:12:54 +0530

    setup eslint in the project

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.node },
+    },
+    tseslint.configs.recommended,
+
+    {
+        ignores: [
+            "dist/**/*",
+            "node_modules/**/*",
+            "build/**/*",
+            "public/**/*",
+            "eslint.config.mjs",
+        ],
+    },
+    {
+        rules: {
+            "no-unused-vars": "error",
+            "no-var": "error",
+        },
+    },
+]);
